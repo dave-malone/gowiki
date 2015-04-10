00c3d0423297d5a5ccaf4ea897d6fb9b1dc512df
David Malone <dmalone@pivotal.io>
2015-04-10 16:24:48 -0400

    added new 'person' API to the program along with db persistence of newly posted people

---
+++ readme.txt
@@ -0,0 +1,5 @@
+running the wiki program:
+
+go build wiki.go
+
+./wiki -dburl=root:gowiki@(192.168.59.103:3306)/gowiki
--- wiki.go
+++ wiki.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"regexp"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
 	templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
 	validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 	addr      = flag.Bool("addr", false, "find open address and print to final-port.txt")
+	dbUrl     = flag.String("dburl", "gowiki@localhost:3306/gowiki", "specify the MySQL database url to connect against")
 )
 
 type Page struct {
@@ -21,9 +27,60 @@ type Page struct {
 	Body  []byte
 }
 
+type Person struct {
+	Id       int    `json:"id,omitempty"`
+	Name     string `json:name`
+	Age      int32  `json:age`
+	EyeColor string `json:eyeColor`
+}
+
+func (p *Person) save() (err error) {
+	db := GetDb()
+	_, err = db.Exec("INSERT INTO PERSON (NAME, AGE, EYE_COLOR) VALUES (?, ?, ?)", p.Name, p.Age, p.EyeColor)
+	return
+}
+
 func (p *Page) save() error {
 	filename := "data/" + p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func GetDb() *sql.DB {
+	db, err := sql.Open("mysql", *dbUrl)
+
+	if err != nil {
+		fmt.Printf("Failed to connect to db %s\n", dbUrl)
+		panic(err.Error())
+	}
+
+	if err := db.Ping(); err == nil {
+		fmt.Printf("Connected to db %s\n", *dbUrl)
+	} else {
+		fmt.Printf("Failed to connect to db %s\n", dbUrl)
+		panic(err.Error())
+	}
+
+	return db
+}
+
+func InitDb() *sql.DB {
+	db := GetDb()
+
+	_, err := db.Exec(
+		`CREATE TABLE PERSON (
+			ID int NOT NULL AUTO_INCREMENT,
+			NAME varchar(255) NOT NULL,
+			AGE int,
+			EYE_COLOR varchar(255),
+			PRIMARY KEY (ID)
+		) `,
+	)
+
+	if err != nil {
+		fmt.Println("Failed to create the PERSON table:", err)
+	}
+
+	return db
 }
 
 func loadPage(title string) (*Page, error) {
@@ -92,12 +149,38 @@ func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
 }
 
+func personHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		decoder := json.NewDecoder(req.Body)
+		var person Person
+		err := decoder.Decode(&person)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Invalid Person request: %v", err), http.StatusInternalServerError)
+		}
+
+		log.Printf("Person: %+v", person)
+
+		err = person.save()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to save your person: %v", err), http.StatusInternalServerError)
+		} else {
+			fmt.Fprintf(w, "We saved %s for you", person.Name)
+		}
+	} else {
+		http.Error(w, fmt.Sprintf("personHandler currently does not support %s requests", req.Method), http.StatusInternalServerError)
+	}
+
+}
+
 func main() {
 	flag.Parse()
+
+	InitDb()
 
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/person/", personHandler)
 	http.HandleFunc("/", homePageHandler)
 
 	if *addr {
